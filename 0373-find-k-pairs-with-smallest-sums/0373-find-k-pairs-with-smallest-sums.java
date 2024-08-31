ba0a9000d02112fdbea94a243bb656c1214a6590
Aditya Prakash Chaudhary <91679895+Adityaachaudhary@users.noreply.github.com>
2024-09-01 02:29:56 +0530

    Time: 29 ms (85.69%), Space: 58.6 MB (69.52%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,36 +1,27 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> resV = new ArrayList<>();
-         // Result list to store the pairs
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        // Priority queue to store pairs with smallest sums, sorted by the sum
-
-        // Push the initial pairs into the priority queue
-        for (int x : nums1) {
-            pq.offer(new int[]{x + nums2[0], 0});
-             // The sum and the index of the second element in nums2
+        List<List<Integer>> res=new ArrayList<List<Integer>>();
+        PriorityQueue<int[]> pq=new PriorityQueue<int[]>((a,b)->(a[0]+a[1])-(b[0]+b[1]));
+        if(nums1.length==0 || nums2.length==0 || k==0){
+            return res;
         }
-
-        // Pop the k smallest pairs from the priority queue
-        while (k > 0 && !pq.isEmpty()) {
-            int[] pair = pq.poll();
-            int sum = pair[0]; // Get the smallest sum
-            int pos = pair[1]; // Get the index of the second element in nums2
-
-            List<Integer> currentPair = new ArrayList<>();
-            currentPair.add(sum - nums2[pos]);
-            currentPair.add(nums2[pos]);
-            resV.add(currentPair); // Add the pair to the result list
-
-            // If there are more elements in nums2,
-            // push the next pair into the priority queue
-            if (pos + 1 < nums2.length) {
-                pq.offer(new int[]{sum - nums2[pos] + nums2[pos + 1], pos + 1});
+        for(int i=0;i<nums1.length && i<k ;i++){
+            pq.offer(new int[]{nums1[i],nums2[0],0});
+        }
+        while(k>0 && !pq.isEmpty()){
+            int curr[]=pq.poll();
+            int e1=curr[0];
+            int e2=curr[1];
+            if(curr[2]+1<nums2.length){
+                pq.offer(new int[]{e1,nums2[curr[2]+1],curr[2]+1});
             }
-
-            k--; // Decrement k
+            List<Integer> temp=new ArrayList<>();
+            temp.add(e1);
+            temp.add(e2);
+            res.add(temp);
+            k--;
         }
+        return res;
 
-        return resV; // Return the k smallest pairs
     }
 }
