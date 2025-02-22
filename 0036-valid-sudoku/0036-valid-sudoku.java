b9d9242e688e85f613587ab5eaf7dc56b5e95af8
Aditya Prakash Chaudhary <91679895+Adityaachaudhary@users.noreply.github.com>
2025-02-22 20:16:07 +0530

    Time: 2 ms (76.29%), Space: 44.9 MB (33.63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,27 +1,47 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet<>();
+         int N = 9;
 
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                char number = board[i][j];
+        // Use hash set to record the status
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+        for (int r = 0; r < N; r++) {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
 
-                if (number != '.') {
-                    if (seen.contains(number + "_R_" + i) ||
-                        seen.contains(number + "_C_" + j) ||
-                        seen.contains(number + "_B_" + (i / 3) + "_" + (j / 3))) {
-                        return false;
-                    } else {
-                        seen.add(number + "_R_" + i);
-                        seen.add(number + "_C_" + j);
-                        seen.add(number + "_B_" + (i / 3) + "_" + (j / 3));
-                    }
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                char val = board[r][c];
+
+                // Check if the position is filled with number
+                if (val == '.') {
+                    continue;
                 }
+
+                // Check the row
+                if (rows[r].contains(val)) {
+                    return false;
+                }
+                rows[r].add(val);
+
+                // Check the column
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx].contains(val)) {
+                    return false;
+                }
+                boxes[idx].add(val);
             }
         }
         return true;
     }
+    
 }
